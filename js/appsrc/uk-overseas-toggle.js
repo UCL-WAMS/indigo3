12a675753da0e0721ebc0a7df104fdb96a9fb1ce
benjiebeetle <ben@eveling.com>
2021-09-10 11:26:40 +0100

    Adapt new toggle app to work with Drupal behaviours

--- js/appsrc/uk-overseas-toggle.js
+++ js/appsrc/uk-overseas-toggle.js
@@ -5,11 +5,15 @@ programme content types in the Indigo3 prospectus. It will be extended to
 cover the UG programme content type when that becomes an indigo 3 type.
 *
 */
-$(document).ready(function() {
-    // First line should be done by CSS, but for now...
-    $(".international").css("display", "none");
-    $(".uk-overseas-button").on("click", function() {
-        $(".uk-overseas").fadeToggle();
-        $(".uk-overseas-button").toggleClass("active");
-    });
-});
+(function ($) {
+    Drupal.behaviors.ucl_indigo_uk_toggle = {
+        attach: function (context, settings) {
+            // First line should be done by CSS, but for now...
+            $(".international").css("display", "none");
+            $(".uk-overseas-button").on("click", function() {
+                $(".uk-overseas").fadeToggle();
+                $(".uk-overseas-button").toggleClass("active");
+            });
+        }
+    }
+})(jQuery, Drupal);
