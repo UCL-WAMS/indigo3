5bd17f9e3870dc8cb60305ee99597acf1af24729
Semhar Tesfu <ccaeste@ucl.ac.uk>
2022-03-04 11:05:08 +0000

    refactor code. Remove document.ready

--- js/app/eu-cookie-module.js
+++ js/app/eu-cookie-module.js
@@ -12,6 +12,7 @@
         var para = $(this).siblings(".eu-cookie-compliance-category-description").first();
         $(para).toggle();
       }
+
       function initCookieSettings() {
         if ($("#eu-cookie-compliance-settings_show-hide").css("display") !== "none") {
           $("#eu-cookie-compliance-settings_show-hide").css("display", "none");
@@ -21,13 +22,11 @@
         }
       }
 
-      $(document).ready(function() {
-        initCookieSettings();
-        $("#sliding-popup").off("click", "#manage-cookie-btn", cookieShowHide).on("click", "#manage-cookie-btn", cookieShowHide);
-        $("#sliding-popup").off("click", ".toggle-text", cookieDescShowHide).on("click", ".toggle-text", cookieDescShowHide);
-        $("#sliding-popup").off("click", "button.eu-cookie-withdraw-tab", initCookieSettings).on("click", "button.eu-cookie-withdraw-tab", initCookieSettings);
-      });
+      $("#sliding-popup").off("click", "#manage-cookie-btn", cookieShowHide).on("click", "#manage-cookie-btn", cookieShowHide);
+      $("#sliding-popup").off("click", ".toggle-text", cookieDescShowHide).on("click", ".toggle-text", cookieDescShowHide);
+      $("#sliding-popup").off("click", "button.eu-cookie-withdraw-tab", initCookieSettings).on("click", "button.eu-cookie-withdraw-tab", initCookieSettings);
+
+      initCookieSettings();
     }
   };
 })(jQuery, Drupal);
-
