ada843da4684a4a8fbe607fa9cbe0f8c0ad356f0
benjiebeetle <ben@eveling.com>
2024-01-17 09:55:14 +0000

    Open UG tab with Int eq dropdown onload

--- js/appsrc/prospectus-int-eq-loader-fix.js
+++ js/appsrc/prospectus-int-eq-loader-fix.js
@@ -3,6 +3,9 @@
         attach: function (context, settings) {
             $(document).ready(function() {
                 if ($("#edit-ucl-international-equivalencies").val() && !$("#international-equivalencies-content").html().trim()) {
+                    if ($("a[href='#tab3-other']").is("a")) {
+                        $("a[href='#tab3-other']").trigger("click");
+                    }
                     $("#edit-ucl-international-equivalencies").trigger("change");
                     const dropdown = document.getElementById('edit-ucl-international-equivalencies');
                     dropdown.scrollIntoView();
